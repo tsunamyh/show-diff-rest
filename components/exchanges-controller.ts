f6f33fbb9bee06764ffaa1de3c5b273a49b09b63
tsunamy <fathpour.hojat@gmail,com>
2026-01-07 17:29:06 +0330

    commit

--- components/exchanges-controller.ts
+++ components/exchanges-controller.ts
@@ -1,7 +1,9 @@
+import { fetchNobitexPrices } from "./exchanges/tracker/nobitexPriceTracker";
 import { fetchOkexPrices } from "./exchanges/tracker/okexPriceTracker";
 import { fetchWallexPrices } from "./exchanges/tracker/wallexPriceTracker";
 
 let wallexFetched = false;
+let nobitexFetched = false;
 
 async function fetchWallexOnce() {
   if (!wallexFetched) {
@@ -10,14 +12,23 @@ async function fetchWallexOnce() {
   }
 }
 
+async function fetchNobitexOnce() {
+  if (!nobitexFetched) {
+    await fetchNobitexPrices();
+    nobitexFetched = true;
+  }
+}
+
 async function getExchangesOrderBooks() {
   // console.log("Starting price trackers...");
   const wallexOrderbooks = await fetchWallexPrices();
+  const nobitexOrderbooks = await fetchNobitexPrices();
   const okexOrderbooks = await fetchOkexPrices();
   // console.log("okexorderbooks:",okexOrderbooks);
   
   return {
     wallexOrderbooks,
+    nobitexOrderbooks,
     okexOrderbooks
   }
 }
