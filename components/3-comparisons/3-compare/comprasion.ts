ad209912a2d5a828a24e02357135c6e4baedb7dc
tsunamy <fathpour.hojat@gmail,com>
2026-02-03 01:04:02 +0330

    commit

--- components/3-comparisons/3-compare/comprasion.ts
+++ components/3-comparisons/3-compare/comprasion.ts
@@ -15,59 +15,59 @@ initOkexHistory();
 initNobitexHistory();
 
 async function intervalFunc(): Promise<NodeJS.Timeout> {
-    return setInterval(async function () {
-        try {
-            const [binanceOrderbooksPromise, exchangesOrderbooksPromise] = await getAllexchangesOrderBooks();
-            let binanceOrderbooks: BinanceOrderbooks;
-            let wallexOrderbooks: WallexOrderbooks;
-            let okexOrderbooks: OkexOrderbooks;
-            let nobitexOrderbooks: NobitexOrderbooks;
+  return setInterval(async function () {
+    try {
+      const [binanceOrderbooksPromise, exchangesOrderbooksPromise] = await getAllexchangesOrderBooks();
+      let binanceOrderbooks: BinanceOrderbooks;
+      let wallexOrderbooks: WallexOrderbooks;
+      let okexOrderbooks: OkexOrderbooks;
+      let nobitexOrderbooks: NobitexOrderbooks;
 
-            if (binanceOrderbooksPromise.status === "rejected") {
-                console.error('Error fetching order books:', {
-                    binanceError: binanceOrderbooksPromise.status === "rejected" ? binanceOrderbooksPromise.reason : null,
-                });
-                return;
-            }
-            if (binanceOrderbooksPromise.status === "fulfilled") {
-                if (!binanceOrderbooksPromise.value) return;
-                binanceOrderbooks = binanceOrderbooksPromise.value;
+      if (binanceOrderbooksPromise.status === "rejected") {
+        console.error('Error fetching order books:', {
+          binanceError: binanceOrderbooksPromise.status === "rejected" ? binanceOrderbooksPromise.reason : null,
+        });
+        return;
+      }
+      if (binanceOrderbooksPromise.status === "fulfilled") {
+        if (!binanceOrderbooksPromise.value) return;
+        binanceOrderbooks = binanceOrderbooksPromise.value;
 
-            }
-            if (exchangesOrderbooksPromise.status === "fulfilled") {
-                if (!exchangesOrderbooksPromise.value) return;
-                wallexOrderbooks = exchangesOrderbooksPromise.value.wallexOrderbooks;
-                const wallexTopRowsInfo = await wallex_priceComp(binanceOrderbooks, wallexOrderbooks);
-                okexOrderbooks = exchangesOrderbooksPromise.value.okexOrderbooks;
-                const okexTopRowsInfo = await okex_priceComp(binanceOrderbooks, okexOrderbooks);
-                nobitexOrderbooks = exchangesOrderbooksPromise.value.nobitexOrderbooks;
-                const nobitexTopRowsInfo = await nobitex_priceComp(binanceOrderbooks, nobitexOrderbooks);
-                
-                const combinedTopRowsInfo = [...wallexTopRowsInfo, ...okexTopRowsInfo, ...nobitexTopRowsInfo];
-                combinedTopRowsInfo.sort((a, b) => b.rowData.percent - a.rowData.percent)
-                const combinedTopRowsInfo10 = combinedTopRowsInfo.slice(0, 22);
+      }
+      if (exchangesOrderbooksPromise.status === "fulfilled") {
+        if (!exchangesOrderbooksPromise.value) return;
+        wallexOrderbooks = exchangesOrderbooksPromise.value.wallexOrderbooks;
+        const wallexTopRowsInfo = await wallex_priceComp(binanceOrderbooks, wallexOrderbooks);
+        okexOrderbooks = exchangesOrderbooksPromise.value.okexOrderbooks;
+        const okexTopRowsInfo = await okex_priceComp(binanceOrderbooks, okexOrderbooks);
+        nobitexOrderbooks = exchangesOrderbooksPromise.value.nobitexOrderbooks;
+        const nobitexTopRowsInfo = await nobitex_priceComp(binanceOrderbooks, nobitexOrderbooks);
 
-                eventEmmiter.emit("diff", JSON.stringify(combinedTopRowsInfo10));
-                const wallexTopFives = getDataByPeriod('wallex');
-                eventEmmiter.emit("diff", JSON.stringify({
-                    status: "maxDiff",
-                    maxDiff: wallexTopFives
-                }));
-                const okexTopFives = getDataByPeriod('okex');
-                eventEmmiter.emit("diff", JSON.stringify({
-                    status: "maxDiff",
-                    maxDiff: okexTopFives
-                }));
-                const nobitexTopFives = getDataByPeriod('nobitex');
-                eventEmmiter.emit("diff", JSON.stringify({
-                    status: "maxDiff",
-                    maxDiff: nobitexTopFives
-                }));
-            }
-        } catch (error) {
-            console.error('Error in priceComp:', error);
-        }
-    }, 10000);
+        const combinedTopRowsInfo = [...wallexTopRowsInfo, ...okexTopRowsInfo, ...nobitexTopRowsInfo];
+        combinedTopRowsInfo.sort((a, b) => b.rowData.percent - a.rowData.percent)
+        const combinedTopRowsInfo10 = combinedTopRowsInfo.slice(0, 22);
+
+        eventEmmiter.emit("diff", JSON.stringify(combinedTopRowsInfo10));
+        const wallexTopFives = getDataByPeriod('wallex');
+        eventEmmiter.emit("diff", JSON.stringify({
+          status: "maxDiff",
+          maxDiff: wallexTopFives
+        }));
+        const okexTopFives = getDataByPeriod('okex');
+        eventEmmiter.emit("diff", JSON.stringify({
+          status: "maxDiff",
+          maxDiff: okexTopFives
+        }));
+        const nobitexTopFives = getDataByPeriod('nobitex');
+        eventEmmiter.emit("diff", JSON.stringify({
+          status: "maxDiff",
+          maxDiff: nobitexTopFives
+        }));
+      }
+    } catch (error) {
+      console.error('Error in priceComp:', error);
+    }
+  }, 10000);
 }
 
 export { intervalFunc, eventEmmiter };
